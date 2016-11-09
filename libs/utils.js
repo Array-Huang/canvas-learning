3f94d0e95efe625b75a8310396ac9c59af8c61a9
Array Huang <hyw125@qq.com>
2016-11-09 16:37:41 +0800

    1

--- libs/utils.js
+++ libs/utils.js
@@ -1,11 +1,12 @@
+/* 计算鼠标或触摸在某元素内部的坐标 */
 var calculateCoorInElement = function(originCoor, element) {
-  var x, y;
+  var x, y; // 整个文档的坐标
   if (originCoor.pageX || originCoor.pageY) {
     x = originCoor.pageX;
     y = originCoor.pageY;
-  } else {
-    x = originCoor.clientX + document.body.scrollLeft +document.documentElement.scrollLeft;
-    y = originCoor.clientY + document.body.scrollTop +document.documentElement.scrollTop;
+  } else { // 兼容IE系列浏览器
+    x = originCoor.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+    y = originCoor.clientY + document.body.scrollTop + document.documentElement.scrollTop;
   }
 
   return {
@@ -23,7 +24,7 @@ var utils = {
 
   /* 获取鼠标在画布内的坐标 */
   getMouseCoordinate: function(element) {
-    if (utils.mouseCoorInited) {
+    if (utils.mouseCoorInited) { // 单例模式
       return utils.mouseCoor;
     }
 
@@ -48,7 +49,7 @@ var utils = {
 
   /* 获取触摸相关的属性 */
   captureTouch: function(element) {
-    if (utils.touchInited) {
+    if (utils.touchInited) { // 单例模式
       return utils.touch;
     }
 
@@ -63,7 +64,8 @@ var utils = {
     });
 
     element.addEventListener('touchmove', function(evt) {
-      var coor = calculateCoorInElement(evt.touches[0], evt.target);
+      var touch = evt.touches[0]; // 只考虑单点触控，因此取数组第一个元素
+      var coor = calculateCoorInElement(touch, evt.target);
       utils.touch.x = coor.x;
       utils.touch.y = coor.y;
     });
